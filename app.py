0a99c4d50bdc4b5a26bec9717c4c6243b0023b16
Olutayo0910 <121323757+Olutayo0910@users.noreply.github.com>
2023-12-18 20:36:51 +0000

    Added html

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'title': 'Data Scientist',
+    'location': 'New York',
+    'salary': 'NGN 500,000'
+  },
+  {
+    'title': 'Software Engineer',
+    'location': 'London',
+    'salary': 'NGN 750,000'
+  },
+  {
+    'title': 'Data Analyst',
+    'location': 'Tokyo',
+    'salary': 'NGN 1,000,000'
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return "Hello, Vic"
+  return render_template('home.html',
+                         jobs=JOBS,
+                        company='Linters')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
